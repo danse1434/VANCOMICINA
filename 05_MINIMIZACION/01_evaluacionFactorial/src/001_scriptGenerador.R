c1d9d406d46158f5ca0edb7e50f185a79901820d
Daniel Parra <danse1434@hotmail.com>
2021-01-29 10:33:02 -0500

    Evaluacion de convergencia de modelo base 1

---
+++ 05_MINIMIZACION/01_evaluacionFactorial/src/001_scriptGenerador.R
@@ -0,0 +1,93 @@
+##------------------------------------------------------------------------------#
+## Nombre del Script: Generación de directorios de trabajos para análisis -------
+##      factorial de convergencia del modelo 
+##
+## Propósito del Script: realizar una estimación de la influencia de valores 
+##      iniciales en la convergencia del modelo base
+##  
+## Autor: Daniel S. Parra González 
+## Fecha de creación:  25-08-2020
+##  
+## Copyright (c) Daniel S. Parra, 2020 
+##  
+## Email: dsparrag@unal.edu.co 
+##------------------------------------------------------------------------------#
+# Carga de paquetes
+require(rlang)
+require(tidyverse)
+#
+# Leer archivo *csv con parámetros de modelo base 
+parametrosPoblacionales <- read_csv(file.path(
+    "..", "..", "04_CORRELACION", "M2CPTM_nobs_2_aditv_corr2", 
+    "populationParameters.txt"
+  ))
+
+pop_par <- parametrosPoblacionales %>% 
+  mutate(minvalue = 0.5 * value,
+         maxvalue = 1.5 * value) %>% 
+  filter(parameter %in% c('Cl_pop', 'V1_pop', 'Q_pop', 'V2_pop')) %>%
+  select(parameter, value, minvalue, maxvalue) %>%
+  t(.)
+
+A <- apply(pop_par[-1,], 2, as.numeric)
+colnames(A) <- pop_par[1,]
+A
+
+B <- expand.grid(
+  Factor1 = A[, 1],
+  Factor2 = A[, 2],
+  Factor3 = A[, 3],
+  Factor4 = A[, 4]
+)
+
+colnames(B) <- pop_par[1,]
+# Matriz con re-arreglos
+B
+write_csv(B, file.path('results', '001_arregloOrigen.csv'))
+# 
+#-------------------------------------------------------------------------------#
+# Creación de archivos de -------------------------------------------------
+#-------------------------------------------------------------------------------#
+fileName <- 'Base_corr2.mlxtran'
+Z = readChar(fileName, file.info(fileName)$size)
+
+Z1 <-
+  # Sólo ejecutar la estimación de parámetros por SAEM
+  str_replace(
+    Z,
+    regex('(?<=\\[TASKS\\]).+(?=\\[SETTINGS\\])', dotall = TRUE),
+    '\r\npopulationParameters()\r\n'
+  ) %>%
+  # Cambio de configuración del algoritmo SAEM
+  str_replace('(?<=burniniterations\\s\\=\\s)\\d{1,6}', "50") %>%
+  str_replace('(?<=smoothingiterations\\s\\=\\s)\\d{1,6}', "2000") %>%
+  str_replace('(?<=exploratoryiterations\\s\\=\\s)\\d{1,6}', "2000") %>%
+  str_replace('(?<=simulatedannealingiterations\\s\\=\\s)\\d{1,6}', "100") %>%
+  str_replace('(?<=exploratoryinterval\\s\\=\\s)\\d{1,6}', "1000") %>%
+  str_replace('(?<=smoothinginterval\\s\\=\\s)\\d{1,6}', "1000") %>% 
+  # Cambio de ruta de modelo
+  str_replace_all("(?<=file\\s\\=\\s)\\'(?=model\\/model\\.txt\\')", "'../../") %>% 
+  # Cambio de ruta de archivo de datos
+  str_replace_all("data/data_TAD.csv", "../../data/data_TAD.csv")
+
+# Creación de directorio principal
+dir.create('evaluacion')
+
+for (i in 1:dim(B)[1]) {
+  # Directorio auxiliar
+  directorioAux <- file.path('evaluacion', paste0('E', i))
+  # Crear directorio auxiliar
+  dir.create(directorioAux)
+  # Cambiar los valores iniciales de la función
+  Y <- Z1 %>%
+    str_replace("(?<=Cl\\_pop\\s\\=\\s\\{value\\=)\\d+\\.\\d+(?=\\,\\smethod)",
+                paste(B[i, 'Cl_pop'])) %>%
+    str_replace("(?<=V1\\_pop\\s\\=\\s\\{value\\=)\\d+\\.\\d+(?=\\,\\smethod)",
+                paste(B[i, 'V1_pop'])) %>%
+    str_replace("(?<=Q\\_pop\\s\\=\\s\\{value\\=)\\d+\\.\\d+(?=\\,\\smethod)",
+                paste(B[i, 'Q_pop'])) %>%
+    str_replace("(?<=V2\\_pop\\s\\=\\s\\{value\\=)\\d+\\.\\d+(?=\\,\\smethod)",
+                paste(B[i, 'V2_pop']))
+  # Escribir archivos
+  write_lines(Y, file.path(directorioAux, fileName), sep = '\n')
+}
---
+++ 05_MINIMIZACION/01_evaluacionFactorial/src/003_ejecucionFIM.R
@@ -0,0 +1,66 @@
+##------------------------------------------------------------------------------#
+## Nombre del Script: Ejecución de algoritmo FIM para SE de estimaciones -------
+##  
+## Propósito del Script:  calcular FIM para SE en estimaciones
+##  
+## Autor: Daniel S. Parra González 
+## Fecha de creación:  22-08-2020
+##  
+## Copyright (c) Daniel S. Parra, 2020 
+##  
+## Email: dsparrag@unal.edu.co 
+##------------------------------------------------------------------------------#
+monolix2019R2.path <-  "C:/ProgramData/Lixoft/MonolixSuite2019R2"
+require(lixoftConnectors, lib.loc = monolix2019R2.path )
+# require(mlxR)
+# initMlxR(path = monolix2019R2.path)   #(Adapte si es necesario).
+initializeLixoftConnectors()
+require(glue)
+require(tidyverse)
+require(progress)
+
+# Selección directorio auxiliar
+aux_dir <- file.path(getwd(), 'evaluacion')
+
+# Modificación de 
+for (i in 1:81) {
+  subDir = glue('{aux_dir}/E{i}/Base_corr2.mlxtran')
+  A = readChar(subDir, file.info(subDir)$size)
+  # Modificar el archivo de control - adicionar gráficos con observaciones
+  B <- A %>%
+    str_replace_all(
+      "(?<=populationParameters\\(\\))\\r\\n",
+      '\r\nplotResult(run = false, method = {saemresults})\r\n'
+    )
+  # Sobrescribir el archivo de control
+  write_lines(B, subDir)
+}
+
+# # Eliminación de carpetas para compatibilidad proyecto
+# for (i in 1:10) {
+#   subdir = glue('{aux_dir}/evaluacion{i}/M2CPTM_nobs_1_prop')
+#   unlink(glue('{subdir}/FisherInformation'), TRUE, TRUE)
+#   unlink(glue('{subdir}/IndividualParameters'), TRUE, TRUE)
+#   unlink(glue('{subdir}/Tests'), TRUE, TRUE)
+# }
+
+N = 81
+ptm <- proc.time()
+
+pb <-
+  progress_bar$new(total = N, format = "[:bar] :percent :percent in :elapsed eta: :eta")
+
+for (i in 64:N) {
+  pb$tick()
+  loadProject(glue('{aux_dir}/E{i}/Base_corr2.mlxtran'))
+  runPopulationParameterEstimation()
+  runStandardErrorEstimation()
+  scenario = getScenario()
+  scenario$plotList = c("saemresults")
+  setScenario(scenario)
+  computeChartsData()
+  
+  # print(glue::glue("Lista la carpeta {i}"))
+}
+
+print(proc.time() - ptm)
